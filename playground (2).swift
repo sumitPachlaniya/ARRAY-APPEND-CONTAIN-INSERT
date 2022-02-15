fd953018487c8cb11b01de600efa7c98b3718fe6
Sumit Pachlaniya <97600179+sumitPachlaniya@users.noreply.github.com>
2022-02-15 10:04:10 -0800

    Add files via upload

---
+++ playground (2).swift
@@ -0,0 +1,13 @@
+var names = ["Sumit","Ronit","Sandip","Tushar"]
+names.append("Sumit")
+    print(names)
+if names.contains("Ronit")
+{
+    print("TRUE")
+}
+else
+{
+    print("FALSE")
+}
+names.insert("Harsh" ,at:2)
+print(names)
